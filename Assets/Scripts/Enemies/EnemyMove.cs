f48b3c6894a7107718f2f00f57dfa3069227dacf
Hanna Halka <hanna.dm.galka@gmail.com>
2026-06-30 16:07:48 +0300

    added gitignjr ang enemy

--- Assets/Scripts/Enemies/EnemyMove.cs
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -6,19 +6,22 @@ public class EnemyMove : MonoBehaviour {
     [SerializeField] private MapGenerator mapGenerator;
 
     [Header("Path")]
-    [SerializeField] private int targetPlayerId = 0;
+    [SerializeField] private int targetPlayerId = 1;
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float heightAboveGround = 0.35f;
 
-    private IEnumerator Move() {
+    private IEnumerator Start() {
+        yield return null;
+
         if (mapGenerator == null) yield break;
 
-        Vector2Int startCell = new Vector2Int(0, 0);
+        Vector2Int startCell = mapGenerator.GetCastlePosition(0);
         Vector2Int targetCell = mapGenerator.GetCastlePosition(targetPlayerId);
 
-        if (startCell.x < 0) yield break;
+        if (startCell.x < 0 || targetCell.x < 0) yield break;
+        
         List<Vector2Int> path = mapGenerator.FindPath(startCell, targetCell);
         if (path.Count == 0 && startCell != targetCell) yield break;
 
