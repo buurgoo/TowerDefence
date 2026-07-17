4a3daf1b049d958317a30257569f8e43a46502cc
D Hill <daaaniel.hill@gmail.com>
2026-07-17 17:20:18 +0300

    add gold counter + ui

---
+++ Assets/Scripts/Inventory/GoldUI.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GoldUI : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Inventory playerInventory; 
+    [SerializeField] private TextMeshProUGUI goldText;
+
+    void Start()
+    {
+        if (playerInventory != null)
+        {
+            playerInventory.Gold.OnValueChanged += OnGoldChanged;
+            
+            UpdateGoldDisplay(playerInventory.Gold.Value);
+        }
+    }
+
+    private void OnGoldChanged(int previousValue, int newValue)
+    {
+        UpdateGoldDisplay(newValue);
+    }
+
+    private void UpdateGoldDisplay(int currentGold)
+    {
+        if (goldText != null)
+        {
+            goldText.text = $"Gold: {currentGold}";
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (playerInventory != null)
+        {
+            playerInventory.Gold.OnValueChanged -= OnGoldChanged;
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Inventory : NetworkBehaviour
+{
+    public NetworkVariable<int> Gold = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    [SerializeField] private float goldGenerationInterval = 1f;
+    [SerializeField] private int goldPerTick = 1;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            StartCoroutine(GenerateGoldOverTime());
+        }
+    }
+
+    private IEnumerator GenerateGoldOverTime()
+    {
+        while (IsServer)
+        {
+            yield return new WaitForSeconds(goldGenerationInterval);
+            Gold.Value += goldPerTick;
+        }
+    }
+
+    public bool TrySpendGold(int amount)
+    {
+        if (!IsServer) return false;
+
+        if (Gold.Value >= amount)
+        {
+            Gold.Value -= amount;
+            return true;
+        }
+        return false;
+    }
+}
